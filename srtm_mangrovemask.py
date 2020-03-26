f236774a1f06870ebd62e0604c85057a57ba4002
Allison Bailey <allison.bailey@stanford.edu>
2020-03-25 17:56:16 -0700

    Add two mangrove layers and error handling to mangrove mask script

--- srtm_mangrovemask.py
+++ srtm_mangrovemask.py
@@ -35,17 +35,24 @@ def main(in_raster_file, mask_file, out_raster_file):
     with fiona.open(mask_file, "r") as mask_file:
         shapes = [feature["geometry"] for feature in mask_file]
 
-    with rasterio.open(in_raster_file)as src:
-        out_image, out_transform = rasterio.mask.mask(src, shapes, all_touched=True, crop=True)
-        out_meta = src.meta
+    try:
+        with rasterio.open(in_raster_file)as src:
+            out_image, out_transform = rasterio.mask.mask(src, shapes, all_touched=True, crop=True)
+            out_meta = src.meta
 
-    out_meta.update({"driver": "GTiff",
-                     "height": out_image.shape[1],
-                     "width": out_image.shape[2],
-                     "transform": out_transform})
+        out_meta.update({"driver": "GTiff",
+                         "height": out_image.shape[1],
+                         "width": out_image.shape[2],
+                         "transform": out_transform})
 
-    with rasterio.open(out_raster_file, "w", **out_meta) as dest:
-        dest.write(out_image)
+        with rasterio.open(out_raster_file, "w", **out_meta) as dest:
+            dest.write(out_image)
+    except ValueError:
+        # get WindowError if mask does not overlap raster -- just skip these
+        # rasterio.errors.WindowError: windows do not intersect
+        # ValueError: Input shapes do not overlap raster.
+        print("Mask and input raster do not overlap -- no output raster")
+        pass
 
 
 #### ---- SETUP and call main function ----------
@@ -63,18 +70,22 @@ wam_prefix = 'wam'
 # Global Mangrove Watch
 gmw2016_path = os.path.join(data_dir, 'gmw2016_Bahamas_MAR.shp')
 gmw2016_prefix = 'gmw2016'
-# Global Mangrove Forests (Andros)
-gmf_path = os.path.join(data_dir, 'gmf_andros.shp')
-gmf_prefix = 'gmf'
-# TNC Landsat Mangroves
+# TNC Landsat Mangroves (Andros)
 tnc_path = os.path.join(data_dir, 'tnc_mangroves_andros.shp')
 tnc_prefix = 'tnc'
+# Global Mangrove Forests
+gmf_path = os.path.join(data_dir, 'gmf_bahamas_MAR.shp')
+gmf_prefix = 'gmf'
+# NatCap Mangroves for MAR region
+ncmar_path = os.path.join(data_dir, 'natcap_mangrovesV4_MAR.shp')
+ncmar_prefix = 'ncmar'
 
 
 # Get a list of raster files with the hmax or hba prefix in the work directory
 os.chdir(work_dir)
 hmax_rasters = glob.glob("hmax_*.tif")
 hba_rasters = glob.glob("hba_*.tif")
+
 
 # Create output filenames with prefix based on mangrove data source
 wam_hmax_rasters = [add_prefix(r, wam_prefix) for r in hmax_rasters]
@@ -116,3 +127,26 @@ for in_raster, out_raster in zip(hmax_rasters, tnc_hmax_rasters):
 for in_raster, out_raster in zip(hba_rasters, tnc_hba_rasters):
     main(in_raster, tnc_path, out_raster)
 
+##---------  GMF (Bahamas & MAR) and NatCap mangroves (MAR only)
+# Create output filenames with prefix based on mangrove data source
+gmf_hmax_rasters = [add_prefix(r, gmf_prefix) for r in hmax_rasters]
+gmf_hba_rasters = [add_prefix(r, gmf_prefix) for r in hba_rasters]
+
+ncmar_hmax_rasters = [add_prefix(r, ncmar_prefix) for r in hmax_rasters]
+ncmar_hba_rasters = [add_prefix(r, ncmar_prefix) for r in hba_rasters]
+
+# GMF mask of Max Height rasters
+for in_raster, out_raster in zip(hmax_rasters, gmf_hmax_rasters):
+    main(in_raster, gmf_path, out_raster)
+
+# GMF mask of Basal area weighted height rasters
+for in_raster, out_raster in zip(hba_rasters, gmf_hba_rasters):
+    main(in_raster, gmf_path, out_raster)
+
+# ncMAR mask of Max Height rasters
+for in_raster, out_raster in zip(hmax_rasters, ncmar_hmax_rasters):
+    main(in_raster, ncmar_path, out_raster)
+
+# ncMAR mask of Basal area weighted height rasters
+for in_raster, out_raster in zip(hba_rasters, ncmar_hba_rasters):
+    main(in_raster, ncmar_path, out_raster)
