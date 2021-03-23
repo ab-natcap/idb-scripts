13a4e62a9b1f95ae84912423b3e5ed071856975f
Allison Bailey <allison.bailey@stanford.edu>
2021-03-23 15:02:47 -0700

    Scripts to mosaic classified rasters and PlanetScope imagery

---
+++ sargassum/mosaic_planet.py
@@ -0,0 +1,39 @@
+""" mosaic_planet.py
+
+Author: Allison Bailey
+Date: 2021-03-23
+
+Mosaic PlanetScope Imagery by date.
+"""
+
+import os
+import subprocess
+from multiprocessing import Pool
+
+base_dir = r'/Users/arbailey/Google Drive/My Drive/planetscope'
+base_subdir = 'files/PSScene4Band'
+
+# List all the top level directories with PlanetScope Imagery
+psdirlist = [dir for dir in os.listdir(base_dir) if os.path.isdir(os.path.join(base_dir,dir))]
+print(psdirlist)
+top_dirs = [os.path.join(base_dir, dir) for dir in psdirlist]
+# print(top_dirs)
+
+for dir in top_dirs:
+    next_dir = os.path.join(dir, base_subdir)
+    image_dirs = [dir for dir in os.listdir(next_dir)]
+    # print(image_dirs)
+    # print(next_dir)
+    image_files = [os.path.join(next_dir,img_id,'analytic_sr',img_id + '_3B_AnalyticMS_SR.tif') for img_id in image_dirs]
+    # print(image_files)
+    os.chdir(dir)
+    outvrt = 'ps' + image_dirs[0].split('_')[0] + 'analytic_sr.vrt'
+    # Need quotes because there is are spaces in the directory name
+    infiles = "'" + "'"' '"'".join(image_files) + "'"
+    print(outvrt)
+    # print(infiles)
+    os.system('gdalbuildvrt -overwrite ' + outvrt + " " + infiles)
+
+#     date_dir = os.path.join(base_dir, dir)
+#     os.chdir(date_dir)
+#     print(date_dir)
---
+++ sargassum/mosaic_s2classified.py
@@ -0,0 +1,69 @@
+""" mosaic_classified.py
+
+Author: Allison Bailey
+Date: 2021-03-22
+
+Mosaic classified Sargassum Sentinel-2 Tiles by date.
+Specify No Data value (-9999)
+"""
+
+
+import os
+import subprocess
+from multiprocessing import Pool
+from timeit import default_timer as timer
+
+input_dir = r'/Users/arbailey/Google Drive/My Drive/sargassum/s2sr_classified'
+
+gdal_nodata_command = 'gdal_edit.py -a_nodata -9999 {classed_tile}'
+
+# https://courses.spatialthoughts.com/gdal-tools.html#running-commands-in-batch
+def add_nodata(file):
+    input = os.path.join(input_dir, file)
+    # filename = os.path.splitext(os.path.basename(file))[0]
+    # output = os.path.join(input_dir, filename + '.tif')
+    # os.system(gdal_nodata_command.format(classed_tile=input)) # doesn't work with space in filename
+    os.system('gdal_edit.py -a_nodata -9999 ' + '"' + input + '"')
+
+def mosaic2vrt(prefix):
+    start_dir = os.getcwd()
+    os.chdir(input_dir)
+    outfile = prefix + '_mosaic.vrt'
+    infiles = prefix + '*.tif'
+    print(outfile)
+    os.system('gdalbuildvrt -overwrite ' + outfile + " " + infiles)
+    os.chdir(start_dir)
+    # gdalbuildvrt -overwrite wam_srtm_hba_BhmMAR.vrt wam_hba*.tif
+
+
+
+
+if __name__ == '__main__':
+
+    files = [file for file in os.listdir(input_dir) if file.endswith('.tif')]
+
+    prefixes = []
+    for f in files:
+        prefix = f.split('_')[0]
+        if not prefix in prefixes:
+            prefixes.append(prefix)
+    print(prefixes)
+
+    ##--- Add No Data value to TIF files
+    # Multiprocess addition of No Data Value - 5 seconds
+    # start = timer()
+    # p = Pool(4)
+    # p.map(add_nodata, files)
+    # end = timer()
+    # print(end - start)
+    # Single Process - 20 seconds
+    # start = timer()
+    # for file in files:
+    #     add_nodata(file)
+    # end = timer()
+    # print(end - start)
+
+    ##-- Create VRT mosaics from single date classifications
+    p = Pool(4)
+    p.map(mosaic2vrt, prefixes)
+
