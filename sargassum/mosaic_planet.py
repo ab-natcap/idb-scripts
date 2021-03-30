6a315d312eff29fe958c2c1c1fa41a002df510fa
Allison Bailey <allison.bailey@stanford.edu>
2021-03-30 10:14:46 -0700

    updated planet mosaic script for new images

--- sargassum/mosaic_planet.py
+++ sargassum/mosaic_planet.py
@@ -7,8 +7,6 @@ Mosaic PlanetScope Imagery by date.
 """
 
 import os
-import subprocess
-from multiprocessing import Pool
 
 base_dir = r'/Users/arbailey/Google Drive/My Drive/planetscope'
 base_subdir = 'files/PSScene4Band'
@@ -16,14 +14,16 @@ base_subdir = 'files/PSScene4Band'
 # List all the top level directories with PlanetScope Imagery
 psdirlist = [dir for dir in os.listdir(base_dir) if os.path.isdir(os.path.join(base_dir,dir))]
 print(psdirlist)
-top_dirs = [os.path.join(base_dir, dir) for dir in psdirlist]
-# print(top_dirs)
+# top_dirs = [os.path.join(base_dir, dir) for dir in psdirlist]  # All directories
+top_dirs = [os.path.join(base_dir, dir) for dir in psdirlist if "201905" in dir] # subset by criteria
+
+print(top_dirs)
 
 for dir in top_dirs:
     next_dir = os.path.join(dir, base_subdir)
     image_dirs = [dir for dir in os.listdir(next_dir)]
     # print(image_dirs)
-    # print(next_dir)
+    print(next_dir)
     image_files = [os.path.join(next_dir,img_id,'analytic_sr',img_id + '_3B_AnalyticMS_SR.tif') for img_id in image_dirs]
     # print(image_files)
     os.chdir(dir)
@@ -31,9 +31,7 @@ for dir in top_dirs:
     # Need quotes because there is are spaces in the directory name
     infiles = "'" + "'"' '"'".join(image_files) + "'"
     print(outvrt)
-    # print(infiles)
+    print(infiles)
     os.system('gdalbuildvrt -overwrite ' + outvrt + " " + infiles)
 
-#     date_dir = os.path.join(base_dir, dir)
-#     os.chdir(date_dir)
-#     print(date_dir)
+
