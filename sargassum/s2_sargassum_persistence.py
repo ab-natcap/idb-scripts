170e5e5bc7a29a430013353b37b010c2b7f1b866
Allison Bailey <allison.bailey@stanford.edu>
2022-01-03 15:16:22 -0800

    updated sargassum persistence script

--- sargassum/s2_sargassum_persistence.py
+++ sargassum/s2_sargassum_persistence.py
@@ -21,36 +21,41 @@ raster_dir = '/Users/arbailey/Google Drive/My Drive/sargassum/s2toa_classified_v
 # 1=sargassum present, 0=absent, -1=no data (clouds),
 # -9999=pre-masked non-sargassum pixels (deepwater, uplands) --> so reclass to 0
 rasters = [r for r in glob.glob(os.path.join(raster_dir,"*mosaic_nd0.vrt"))]
-print(rasters)
-print(len(rasters))
+rasters.sort()
 
 # test set
-rasters = ['/Users/arbailey/Google Drive/My Drive/sargassum/s2toa_classified_v1/20190211T161411_mosaic_nd0.vrt']
+# rasters = rasters[0:2]
+# rasters = ['/Users/arbailey/Google Drive/My Drive/sargassum/s2toa_classified_v1/20190211T161411_mosaic_nd0.vrt']
+print("Raster list:", rasters)
+print("Count of input rasters ", len(rasters))
 
+reclassed_rasters = []
 for r in rasters:
-    allclasses = rxr.open_rasterio(r).squeeze()
-    print(type(allclasses))
-    print("The CRS for this data is:", allclasses.rio.crs)
-    print("The spatial extent of this data is: ", allclasses.rio.bounds())
-    print("The shape of this data is:", allclasses.shape)
-    print("The no data value is:", allclasses.rio.nodata)
+    print(os.path.basename(r))
+    # Open the raster and squeeze into two dimensions (ignore "band dimension which is 1)
+    allclasses = rxr.open_rasterio(r).squeeze()  # squeeze removes the "band" dimension (which is 1)
+    # print(type(allclasses))
+    # print("The CRS for this data is:", allclasses.rio.crs)
+    # print("The spatial extent of this data is: ", allclasses.rio.bounds())
+    # print("The shape of this data is:", allclasses.shape)
+    # print("The no data value is:", allclasses.rio.nodata)
     # print("the minimum raster value is: ", np.nanmin(allclasses.values))
     # print("the maximum raster value is: ", np.nanmax(allclasses.values))
-    present = allclasses.where(allclasses == 1)
-    # intermediate = allclasses.where(allclasses == -9999, 0) #, 0, allclasses.all())  #, x=0, y=allclasses)
+
+    # # Raster of Sargassum present (=1)  -- This is just an example, used the logic for intermediate, below
+    # present = allclasses.where(allclasses == 1)
+    #
+    # Raster of present/absent (1, 0, -9999 (pre-masked as not Sargassum, so equivalent to 0)
     intermediate = allclasses.where(allclasses != -1)  #, 0, allclasses.all())  #, x=0, y=allclasses)
 
-    print(present)
-    print(np.nanmin(present.values))
-    print(np.nanmax(present.values))
-
-    print(intermediate)
-    print(np.nanmin(intermediate.values))
-    print(np.nanmax(intermediate.values))
-
+    # Reclassify everything that isn't = 1 to zero
+    # So, <1 becomes 0, >= 1 becomes 1
     class_bins = [1]
-
     sargassum = xr.apply_ufunc(np.digitize, intermediate, class_bins)
-    print(sargassum)
-    print(np.nanmin(sargassum.values))
-    print(np.nanmax(sargassum.values))
+    # print("Info from reclassed Sargassum raster:")
+    # print(sargassum)
+    # print("Min value: ",np.nanmin(sargassum.values))
+    # print("Max value: ",np.nanmax(sargassum.values))
+    reclassed_rasters.append(sargassum)
+
+print(len(reclassed_rasters))
